2fc80ea6165d8e0c61e742642aa096705d8b9f0a
Malena8a <maochoa@frba.utn.edu.ar>
2024-04-18 20:13:49 +0000

    clientToKer entradasalida

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -1,8 +1,15 @@
-#include <stdlib.h>
+/*#include <stdlib.h>
 #include <stdio.h>
 #include <utils/hello.h>
 
 int main(int argc, char* argv[]) {
     decir_hola("una Interfaz de Entrada/Salida");
     return 0;
+}*/
+
+#include <clientToKer.h>
+
+int main(int argc, char* argv[]) {
+    int result = clientToKer();
+    return result;
 }
